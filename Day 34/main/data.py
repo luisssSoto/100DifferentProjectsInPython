5b1cd652333525a9ad1a752dc4e5663bf919c219
AlexSoto <alejandrosotoserafin@gmail.com>
2025-09-23 19:08:36 -0500

    add project 34

---
+++ Day 34/main/data.py
@@ -0,0 +1,14 @@
+import requests
+import html.parser
+
+parameters = {
+    "amount":10,
+    "category":18,
+    "difficulty": "easy",
+    "type": "boolean"
+}
+response = requests.get(f"https://opentdb.com/api.php?", params=parameters)
+response.raise_for_status()
+question_data = response.json()
+question_data = question_data["results"]
+
